aa0f73fcbdb6593cfa47c8936f2ce29297104bdf
gbarne2 <georgetbarney@gmail.com>
2015-09-25 15:29:06 -0500

    updated function names and added tx_end_turn()

--- Client/clientTransmitHandler.h
+++ Client/clientTransmitHandler.h
@@ -4,6 +4,7 @@
 int sendPacketTX(int player, char* data, int length, int packet_type);
 int sendPacketTX(int player, string data, int packet_type);
 int sendPacketTX(int player, int data, int packet_type);
-int proposeTrade(int player_to_trade, trade_cards_offer trade);
-int accept_or_reject_trade(int player_to_trade, int status);
-int start_turn();
+int tx_proposeTrade(int player_to_trade, trade_cards_offer trade);
+int tx_accept_or_reject_trade(int player_to_trade, int status);
+int tx_start_turn();
+int tx_end_turn();
